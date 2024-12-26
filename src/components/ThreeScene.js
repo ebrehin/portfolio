507acf2ecf0d057524de7cb4843dd9093ab04578
Ethan Brehin <80754323+ebrehin@users.noreply.github.com>
2024-12-26 20:26:11 +0100

    Transition sans mouvements de caméra

--- src/components/ThreeScene.js
+++ src/components/ThreeScene.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react';
-import { Canvas, useFrame, extend } from '@react-three/fiber';
+import { Canvas, useFrame} from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 import { useNavigate } from 'react-router-dom';
 import * as THREE from 'three';
@@ -13,6 +13,7 @@ export default function ThreeScene() {
   const isIdle = useRef(false);
   const finishIdle = useRef(false);
   const laptopRef = useRef(null);
+  const initialRotation = THREE.MathUtils.degToRad(30);
   const rotationSpeed = 0.5;
   const timeForRotation = 2;
   const targetRotation = useRef(0);
@@ -27,8 +28,14 @@ export default function ThreeScene() {
       action.stop();
     });
 
+    const IdleAction = actions.current['Idle'];
+    IdleAction.paused = true;
+    IdleAction.time = 0; 
+    IdleAction.play();
+    IdleAction.clampWhenFinished = true;
     
     // Arrêter la rotation idle et commencer la rotation finale
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
     isIdle.current = false;
     finishIdle.current = true;
     destinationRef.current = destination;
@@ -36,19 +43,19 @@ export default function ThreeScene() {
     // Calculer la rotation restante pour compléter le tour    
     const currentRotation = laptopRef.current.rotation.y;
     const fullRotations = Math.ceil(currentRotation / (2 * Math.PI));
-    targetRotation.current = fullRotations * 2 * Math.PI;
+    targetRotation.current = initialRotation + fullRotations * 2 * Math.PI;
     remainingRotation.current = targetRotation.current - currentRotation;
   };
 
   const startTransitionAnimations = () => {
     const transitionAction = actions.current['transition'];
     const cylinderAction = actions.current['Cylinder action'];
+    const IdleAction = actions.current['Idle'];
     
-    if (transitionAction && cylinderAction) {
+    if (transitionAction && cylinderAction && IdleAction) {
       transitionAction.setLoop(THREE.LoopOnce);
       transitionAction.clampWhenFinished = true;
       cylinderAction.setLoop(THREE.LoopOnce);
-      cylinderAction.clampWhenFinished = true;
       
       const handleFinished = (e) => {
         if (e.action === transitionAction) {
@@ -61,6 +68,7 @@ export default function ThreeScene() {
       };
 
       mixer.current.addEventListener('finished', handleFinished);
+      IdleAction.stop();
       transitionAction.play();
     } else {
       navigate(destinationRef.current);
@@ -73,7 +81,11 @@ export default function ThreeScene() {
         actions.current[clip.name] = mixer.current.clipAction(clip);
       });
       
-      laptopRef.current.rotation.y = 0;
+      // Initial setup
+      const cylinderAction = actions.current['Cylinder action'];
+      cylinderAction.time = 0; 
+      window.scrollTo(0, 0);
+      laptopRef.current.rotation.y = initialRotation;
       const introAction = actions.current['Intro'];
       if (introAction) {
         introAction.setLoop(THREE.LoopOnce);
@@ -99,7 +111,7 @@ export default function ThreeScene() {
       //console.log("le :",isIdle.current, laptopRef.current.rotation.x, laptopRef.current.rotation.y, laptopRef.current.rotation.z);
       if (finishIdle.current && laptopRef.current) {
         // Calculer le pas de rotation pour ce frame
-        const rotationStep = (2 * Math.PI) / timeForRotation * delta;
+        const rotationStep = (initialRotation + (2 * Math.PI)) / timeForRotation * delta;
         
         // Si la rotation restante est plus petite que le pas, on termine
         if (remainingRotation.current <= rotationStep) {
